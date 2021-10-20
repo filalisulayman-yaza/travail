4b9d5112e2972a5957748f2ebece7072ef478d99
filalisulayman-yaza <91486602+filalisulayman-yaza@users.noreply.github.com>
2021-10-20 16:55:32 +0200

    Add files via upload

---
+++ thonny.py
@@ -0,0 +1,20 @@
+def maximum(a,b):
+    if a < b:
+        return(b)
+    else:
+        return(a)
+maximum(10,16)
+
+def maximum(a,b):
+    if a < b:
+        return(a)
+    else:
+        return(b)
+maximum(10,16)
+
+def valeur_absolue(x,0):
+    if x <= 0:
+        return(x)
+    else:
+        return(0)
+maximum(10,16)
