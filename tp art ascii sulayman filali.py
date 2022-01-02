0f2a8d6bcf0d1d99bb3182284bc2c3781fa8af0d
filalisulayman-yaza <91486602+filalisulayman-yaza@users.noreply.github.com>
2022-01-02 21:08:33 +0100

    Add files via upload

---
+++ tp art ascii sulayman filali.py
@@ -0,0 +1,36 @@
+def afficher_ligne(n):
+    for p in range(n):
+        print("O", end = "")
+        
+def afficher_carre(n):
+    for p in range(n):
+        for x in range(n):
+            print("O", end = "")
+        print()
+        
+def afficher_rectangle(y,x):
+    for p in range(y):
+        for p in range(x):
+            print("O", end = "")
+        print()
+        
+def afficher_triangle_rectangle(n):
+    nombre = 1
+    for p in range(n):
+        for x in range(nombre):
+            print("O", end = "")
+        print()
+        nombre = nombre + 1
+        
+def afficher_carre_diagonale(n):
+    for p in range(n):
+        if p == 0:
+            print("X", end = "")
+            for p in range(n−1):
+
+   
+   
+   
+   
+
+    
